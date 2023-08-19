2f1c70cdee39afb4d00e0cf42f5628303cc8f6fa
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-19 15:35:20 +0530

    Added Animals Controller

---
+++ AnimalShelterAPI/Controllers/AnimalsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using AnimalShelterAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AnimalShelterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly AnimalShelterDbContext _context;
+
+        public AnimalsController(AnimalShelterDbContext context)
+        {
+            _context = context;
+        }      
+    }
+}
