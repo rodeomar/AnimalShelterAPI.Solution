dc30f104012d44ca66d9e8bf79eb65f6d68905f7
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-19 15:07:15 +0530

    Added Dog Model

---
+++ AnimalShelterAPI/Models/Dog.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalShelterAPI.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public bool IsAdopted { get; set; }
+    }
+}
